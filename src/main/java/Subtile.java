f09fa38ee53fcc1247ab8f71a4c49b0ff9e471e6
junkerjoerg12 <jakob-engel@mein.gmx>
2023-08-01 13:43:53 +0200

    Konstruktor für 50* 50 Felder

---
+++ src/main/java/Subtile.java
@@ -0,0 +1,17 @@
+package main.java;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Subtile extends JPanel{
+
+
+    public Subtile(Color farbe){
+        this.setSize(50, 50);
+        this.setBackground(farbe);
+        this.setVisible(true);
+
+
+    }
+
+}
