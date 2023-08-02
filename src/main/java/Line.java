26d846a363ea35765ea69dffad0da50058ab7df9
junkerjoerg12 <jakob-engel@mein.gmx>
2023-08-02 21:42:43 +0200

    Lines Klasse finzugefügt, paint Methode gelöscht

---
+++ src/main/java/Line.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+
+public class Line extends JPanel{
+    
+    public Line(boolean vertical, int start){
+        this.setBackground(Color.BLACK);
+
+        
+    }
+}
--- src/main/java/MyPanel.java
+++ src/main/java/MyPanel.java
@@ -13,24 +13,6 @@ public class MyPanel extends JPanel{
 
     }
 
-    public void paint (Graphics g){
-        Graphics2D g2D =(Graphics2D) g;
-        g2D.setStroke(new BasicStroke(2));
-        g2D.setPaint(Color.BLACK);
 
-
-        //Zeichent vertikate linien des Gitters
-        for(int i=1; i<anzahlSpalten; i++){
-            g2D.drawLine(i*50, 0, i*50, 500);
-        }
-        
-        //Zeichnet horizontale Linien des GItters
-        for(int i=1; i<anzahlZeilen; i++){
-            g2D.drawLine(0,i*50,500,i*50);
-
-        }
-
-
-    }
 
 }
