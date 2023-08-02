9591e43233658bfb30556305766e8f316ce4bde6
junkerjoerg12 <jakob-engel@mein.gmx>
2023-08-02 22:13:01 +0200

    SquareTile Klasse, added Koordinaten zu Subtiles

---
+++ src/main/java/SquareTile.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class SquareTile extends Tile{
+
+
+    
+    
+}
--- src/main/java/Subtile.java
+++ src/main/java/Subtile.java
@@ -5,8 +5,13 @@ import java.awt.*;
 
 public class Subtile extends JPanel{
 
+    int xKoord;
+    int yKoord;
 
-    public Subtile(Color farbe){
+
+    public Subtile(Color farbe, int xKoord, int yKoord){
+        this.xKoord=xKoord;
+        this.yKoord=yKoord;
         this.setSize(50, 50);
         this.setBackground(farbe);
         this.setVisible(true);
