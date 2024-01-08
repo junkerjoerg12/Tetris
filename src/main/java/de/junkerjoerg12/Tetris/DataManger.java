6ad2c2e1a158cebfab016e837208995365853329
junkerjoerg12 <138699069+junkerjoerg12@users.noreply.github.com>
2024-01-08 16:11:59 +0100

    MEthoden zum speichern von nutzerdaten angefangen

--- src/main/java/de/junkerjoerg12/Tetris/DataManger.java
+++ src/main/java/de/junkerjoerg12/Tetris/DataManger.java
@@ -6,9 +6,13 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DataManger {
 
@@ -24,6 +28,85 @@ public class DataManger {
       mngr = new DataManger();
     }
     return mngr;
+  }
+
+  public void writeNutzerHighscore(String user, String modus, int highscore) {
+
+  }
+
+  public int getUserHighscore(String username, String modus) {
+    JSONObject user;
+    String userdata = "";
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(getFilePath(username)));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        userdata += line;
+      }
+      user = new JSONObject(userdata);
+      user.getString(modus);
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (JSONException e) {
+      user = new JSONObject(userdata);
+      user.put(modus, 0);
+      try {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(getFilePath(username)));
+        writer.write(user.toString());
+      } catch (IOException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+      }
+    }
+    return 0;
+  }
+
+  public boolean nutzerAnlegen(String username, String passwort) { // gibt true zurück, wenn ein neuer nutzer erstellt
+                                                                   // wurde und false, falls der nutzer bereits
+                                                                   // existiert
+    Path filePath = Paths.get(getFilePath(username));
+    System.err.println(filePath + " ist der Pfad");
+    if (Files.exists(filePath)) {
+      System.out.println("Ein nutzer mit dem gleichen nutzernamen exiustiert bereits");
+      return false;
+    } else {
+      System.out.println("Nutzer kann erstellt werden");
+      try {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(getFilePath(username)));
+        JSONObject user = new JSONObject();
+        user.append("username", encrypt(username));
+        user.append("passwort", encrypt(passwort));
+        writer.write(user.toString());
+        writer.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return true;
+    }
+  }
+
+  public JSONObject getUserdata(String username) throws FileNotFoundException {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(getFilePath(username)));
+      String line;
+      String file = "";
+      try {
+        while ((line = reader.readLine()) != null) {
+          file += line;
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      return new JSONObject(file);
+
+    } catch (JSONException e) {
+      return new JSONObject("{}");
+    }
   }
 
   public int getHighscore(String modus) throws IOException {// muss in den IMplementierung noch angepasst werden
@@ -80,9 +163,26 @@ public class DataManger {
     }
   }
 
+  private String encrypt(String plainText) {
+    return plainText.hashCode() + "";
+  }
+
+  private String getFilePath(String username) {
+    return USERDATA + "/" + encrypt(username) + ".json";
+  }
+
   public static void main(String[] args) {
 
-    DataManger.getDataManger().checkFolders();
+    try {
+      System.out.println(DataManger.getDataManger().getUserdata("junkerjoerg12"));
+    } catch (FileNotFoundException e) {
+      System.out.println(
+          "Der NUtzer ist noch nicht erstellt, überprüfen sie auf rechtschreibung oder erstellen sie einen neuen Nutzer");
+    }
+    DataManger.getDataManger().nutzerAnlegen("junkerjoerg12", "passwort");
+
+    System.err.println(DataManger.getDataManger().getUserHighscore("junkerjoerg12", "normal"));
+    // DataManger.getDataManger().checkFolders();
 
     // Test zum saven
     // try {
@@ -92,11 +192,11 @@ public class DataManger {
     // }
 
     // Test zum auslesen
-    try {
-      System.out.println(getDataManger().getHighscore("normal") + "Absoluterhighscore");
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    // try {
+    // System.out.println(getDataManger().getHighscore("normal") +
+    // "Absoluterhighscore");
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
   }
 }
